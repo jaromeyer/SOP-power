f0b61edaddba36b22eb1c3760637eb3e80814d19
Jaro <jaro@Jaros-HackBook-Pro.local>
2020-03-30 13:46:59 +0200

    added automated oms saving

--- sop_bot.py
+++ sop_bot.py
@@ -8,7 +8,7 @@ import discord
 from dotenv import load_dotenv
 from PIL import Image
 
-multiplayer = True
+multiplayer = False
 myTurn = True
 
 active = False
@@ -22,21 +22,25 @@ client = discord.Client()
 
 
 def adb_execute(command):
-    os.system('/Users/jaro/Library/Android/sdk/platform-tools/adb ' + command)
+    os.system(r'/Users/jaro/Library/Android/sdk/platform-tools/adb ' + command)
 
 
 async def send_profile(message):
     global active, votes
     time.sleep(0.5)
+    im_count = 1
+    if not os.path.exists('img/temp'):
+        os.makedirs('img/temp')
     while True:
         adb_execute('shell screencap -p /sdcard/DCIM/screenshot.png')
-        adb_execute('pull /sdcard/DCIM/screenshot.png screenshot.png')
+        adb_execute('pull /sdcard/DCIM/screenshot.png img/screenshot.png')
         adb_execute('shell input tap 880 1000')
-        im = Image.open('screenshot.png')
+        im = Image.open('img/screenshot.png')
         pix = im.load()
         im = im.crop((35, 193, 1050, 1687))
-        im.save('cropped.png')
-        await message.channel.send(file=discord.File('cropped.png'))
+        im.save('img/temp/cropped%d.png' % im_count)
+        await message.channel.send(file=discord.File('img/temp/cropped%d.png' % im_count))
+        im_count += 1
         if pix[1020, 202] == (255, 255, 255, 255) or pix[65, 202] != (255, 255, 255, 255):
             active = True
             votes = {}
@@ -53,7 +57,7 @@ async def on_message(message):
             bytes(message.attachments[0].id % 10)).hexdigest(), 16)
         myTurn = img_hash % 2 == 0
         print(message.attachments[0].id, img_hash)
-    if (re.match(r'(Der fette.*|Der Grosse Rat.*)', message.content) and myTurn) or not multiplayer:
+    if (re.match(r'(Der fette.*|Der Grosse Rat.*)', message.content) and myTurn):
         await send_profile(message)
 
     if message.author == client.user:
@@ -62,6 +66,10 @@ async def on_message(message):
     if re.match(r'sop \d+', message.content) and not active:
         players = int(re.findall(r'\d+', message.content)[0])
         myTurn = True
+        if not os.path.exists(r'img'):
+            os.makedirs(r'img')
+        if not os.path.exists(r'img/oms'):
+            os.makedirs(r'img/oms')
         await message.channel.send("SOP started")
         await send_profile(message)
     elif message.content == 'stop' and active:
@@ -87,6 +95,9 @@ async def on_message(message):
                     adb_execute('shell input tap 390 1820')
             elif smash_count > players/2:
                 if smash_count == players:
+                    folder_index = max(
+                        [int(f) for f in next(os.walk('img/oms'))[1]]) + 1
+                    os.rename(r'img/temp', r'img/oms/%i' % folder_index)
                     response = "Der Grosse Rat hat entschieden: **OBERMEGA-SMASH**:peach::eggplant::sweat_drops:"
                 else:
                     response = "Der Grosse Rat hat entschieden: **SMASH**:peach:"
@@ -98,5 +109,7 @@ async def on_message(message):
                     response = "Der Grosse Rat hat entschieden: **PASS**:nauseated_face:"
                 adb_execute('shell input tap 390 1820')
             await message.channel.send(response)
+            if not multiplayer:
+                await send_profile(message)
 
 client.run(TOKEN)
